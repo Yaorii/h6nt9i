be9869e85c79281454030bd37c0d78e697b874f1
Yaorii <48913410+Yaorii@users.noreply.github.com>
2019-03-25 18:11:23 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -18,5 +18,14 @@ bot.on('message', message => {
     
 });
 
+bot.on('guildMemberAdd', join => {
+    let embed = new Discord.RichEmbed()
+        .setAuthor(join.guild.name, join.guild.iconURL)
+        .setColor('468abb')
+        .setDescription('Bienvenue' + join.user.username + 'sur' + join.guild.name)
+        .setFooter('Nous sommes désormais' + join.guild.memberCount)
+    join.guild.channels.get('558690513285414943').send(embed)
+
+});
 
 bot.login('NTU5NzY5MjcwODA5MjY0MTM4.D3qOxg.4aHyyYYqSz-YK3UNi9-7F41EaHE');
