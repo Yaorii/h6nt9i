b558bf2ff4bc2ea6e92e5114d666cc630bf1ab77
Yaorii <48913410+Yaorii@users.noreply.github.com>
2019-03-25 17:48:45 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,10 +1,22 @@
 const Discord = require('discord.js');
 
+var prefix = ".";
+
 var bot = new Discord.Client();
 
 bot.on('ready', function() {
     bot.user.setActivity('le serveur', {type: "WATCHING"});
     console.log("Le bot est bien connecté");
 });
+
+// TEST BOT
+
+bot.on('message', message => {
+    if(message.content === 'Yo'){
+        message.channel.send('Salut');
+    }
+    
+});
+
 
 bot.login('NTU5NzY5MjcwODA5MjY0MTM4.D3qOxg.4aHyyYYqSz-YK3UNi9-7F41EaHE');
